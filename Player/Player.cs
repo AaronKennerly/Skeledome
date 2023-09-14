8300f96f1cf9e8d79a525cdd8fc6c0f7a3296c55
AaronKennerly <kennerlyac@appstate.edu>
2023-09-14 16:51:52 -0400

    Got basic respawn timer to work + jump buff + fixed coyote time and double jump

--- Player/Player.cs
+++ Player/Player.cs
@@ -10,31 +10,69 @@ public partial class Player : CharacterBody2D
 	public const float JumpVelocity = -600.0f;
 	public const float Acceleration = 10.0f; 
 	public const float Decceleration = 20.0f;
+
+	public const float JumpBufferTime = 0.1f;
+	public const float CoyoteTime = 0.2f;
+	public float CoyoteTimer;
+	public float JumpBufferTimer = -10;
+	public bool JumpBool = false;
+
+	public Timer respawnTimer;
 	public Vector2 Force;
 	public float Mass = 60.0f;
 	public int JumpCount = 0;
+	public Vector2 velocity;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
+	public override void _Ready()
+	{
+		respawnTimer = GetNode<Timer>("RespawnTimer");
+		respawnTimer.Timeout += OnTimerTimeout;
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		velocity = Velocity;
+
+		if (JumpBufferTimer > 0f) {
+            JumpBufferTimer -= (float)delta;
+        }
+
+		// handle timer
+        if(IsOnFloor()) {
+            CoyoteTimer = CoyoteTime;
+            JumpCount = 0;
+        } else {
+            CoyoteTimer -= (float)delta;
+        }
 
 		// Add the gravity.
 		if (!IsOnFloor())
 			if (velocity.Y < 0) velocity.Y += gravity * (float)delta;
 			else velocity.Y += gravity * 2 * (float)delta; 
 
+		if (Input.IsActionJustPressed(controls.jump)) {
+			JumpBufferTimer = JumpBufferTime;
+		}
+
 		// Handle Jump.
-		if (Input.IsActionJustPressed(controls.jump) && JumpCount < 1) {
-			velocity.Y = JumpVelocity;
-			JumpCount++;
+		if (Input.IsActionJustPressed(controls.jump) && (CoyoteTimer > 0f || JumpCount < 2)) {
+			if (CoyoteTimer < 0f) JumpCount++;
+			Jump();
 		}
 		
-		// reset jumps
-		if(IsOnFloor()) {
+		if (JumpBufferTimer >= 0f && IsOnFloor()) {
 			JumpCount = 0;
+		 	Jump();
+		 }
+		 
+		 // When jump button is released (Janky solution. Reassess)
+		if(!Input.IsActionPressed(controls.jump) && JumpBool == true) {
+			velocity.Y = velocity.Y + 400.0f;
+			CoyoteTimer = 0f;
+			JumpBool = false;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
@@ -55,19 +93,26 @@ public partial class Player : CharacterBody2D
 		Velocity = velocity;
 		MoveAndSlide();
 		UpdateForce(velocity);
-
-		// this section is to check if the player is out of boounds
-		if (Position.Y >= 750 | Position.X <= -200 | Position.X >= 1350 ) {
-			Die();
-		}
 	}
 	
 	public void UpdateForce(Vector2 velocity) {
 		Force = velocity * Mass;
 	}
 
+	public void Jump() {
+		JumpCount++;
+		velocity.Y = JumpVelocity;
+		JumpBool = true;
+	}
+
 	// this functionis for when the player falls off the map
 	public void Die() {
 		GameManager.respawnPlayer(this);
+	}
+
+	public void OnTimerTimeout() {
+		if (Position.Y >= 750 | Position.X <= -200 | Position.X >= 1350) {
+			Die();
+		}
 	}
 }
