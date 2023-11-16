64ede3fee0cc8a9317ee740297d350493a06bc44
AaronKennerly <kennerlyac@appstate.edu>
2023-11-16 18:17:28 -0500

    Volley fire now works pretty good.

--- Levels/centaur.gd
+++ Levels/centaur.gd
@@ -2,33 +2,37 @@ extends StaticBody2D
 
 var arrow = load("res://Levels/arrow.tscn")
 
-var shooting = true
-var longShotPower = 900.0
+var shooting = false
+const longShotPower = 700.0
 var shotPower = 1000.0
+var shotCount = 0
 @onready var shot_direction = $Shot_Direction
 @onready var firerate = $FireRate
+@onready var volleyrate = $VolleyTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	shoot()
+	pass
 
 
 func shoot():
-	shooting = true
 	firerate.start()
 
 
 func volley():
 	shotPower = longShotPower
 	
-	while(shotPower > 400.0):
-		if(!shooting):
-			shoot()
+	while shooting:
+		shoot()
+		await firerate.timeout
 
 
 func _physics_process(_delta):
-	if (shooting):
+	if (volleyrate.is_stopped() && !shooting):
 		print("test")
+		volleyrate.start()
+		shooting = true
+		volley()
 
 func _on_fire_rate_timeout():
 	var spawned_arrow = arrow.instantiate()
@@ -37,6 +41,14 @@ func _on_fire_rate_timeout():
 	spawned_arrow.speed = shotPower
 	spawned_arrow.global_position = shot_direction.position
 	add_child(spawned_arrow)
-	shotPower -= 150.0
-	shooting = false
+	print(shotPower)
+	shotPower -= 125.0
+	shotCount += 1
+	if (shotCount > 4):
+		shooting = false
+		shotCount = 0
 	
+
+
+func _on_volley_timer_timeout():
+	volleyrate.stop()
